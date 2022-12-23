24529fcf6b73067ea8d60ca3600e59786528a7f5
JP Carey <jpcarey4@gmail.com>
2022-12-23 11:34:21 -0700

    remove photos

--- src/Adventures/adventureImages/imgImports.js
+++ src/Adventures/adventureImages/imgImports.js
@@ -1,369 +1,108 @@
 import warmSprings from './jacksonLake/warmSprings.JPG'
-import graceFishing from './jacksonLake/graceFishing.jpg'
-import desertRiver from './jacksonLake/desertRiver.jpg'
-import sandLookout from './jacksonLake/sandLookout.JPG'
-import lakeEntrance from './jacksonLake/lakeEntrance.jpg'
-import stillPaddle from './jacksonLake/stillPaddle.jpg'
-import babsRainbow from './jacksonLake/babsRainbow.jpg'
-import rainPaddle from './jacksonLake/rainPaddle.JPG'
-import moranSunset from './jacksonLake/moranSunset.JPG'
-import lorraineWave from './jacksonLake/lorraineWave.jpg'
-import graceSelfie from './jacksonLake/graceSelfie.JPG'
 
 import landscape from './middleFork/landscape.jpg'
-import tappandRanch from './middleFork/tappanRanch.jpg'
-import pistol from './middleFork/pistol.JPG'
-import wall from './middleFork/wall.jpg'
-import overlook from './middleFork/overlook.JPG'
-import hikeDown from './middleFork/hikeDown.JPG'
-import boatsOnBeach from './middleFork/boatsOnBeach.JPG'
-import smallRapid from './middleFork/smallRapid.jpg'
-import deepWater from './middleFork/deepWater.jpg'
-import veilFalls from './middleFork/veilFalls.JPG'
-import launchDay from './middleFork/launchDay.JPG'
 
 import shoshoneOpening from './ynpShoshone/shoshoneOpening.jpeg'
-import geyserBasin from './ynpShoshone/geyserBasin.jpeg'
-import heartCauldron from './ynpShoshone/heartCauldron.jpeg'
-import ianFishing from './ynpShoshone/ianFishing.jpeg'
-import kateHungover from './ynpShoshone/kateHungover.jpeg'
-import katePaddling from './ynpShoshone/katePaddling.jpeg'
-import lewisLake from './ynpShoshone/lewisLake.jpeg'
-import lilChannel from './ynpShoshone/lilChannel.jpeg'
-import silasFihing from './ynpShoshone/silasFishing.jpeg'
-import sunrise from './ynpShoshone/sunrise.jpeg'
 
 import yurtMorning from './commissaryRidge/yurtMorning.jpeg'
-import beardsSkin from './commissaryRidge/beardsSkin.jpeg'
-import beardsSummit from './commissaryRidge/beardsSummit.jpeg'
-import crewSkiing from './commissaryRidge/crewSkiing.jpeg'
-import hillaryMe from './commissaryRidge/hillayMe.jpeg'
-import ridgeSkin from './commissaryRidge/ridgeSkin.jpeg'
 
 import sunsetSummit from './hokkaido/sunsetSummit.jpeg'
-import afterSkiing from './hokkaido/afterSkiing.jpeg'
-import andrewKicker from './hokkaido/andrewKicker.jpeg'
-import coleLaughing from './hokkaido/coleLaughing.jpeg'
-import furanoAlley from './hokkaido/furanoAlley.jpeg'
-import henrySmoke from './hokkaido/henrySmoke.jpeg'
-import snowBridge from './hokkaido/snowBridge.jpeg'
-import threeBoys from './hokkaido/threeBoys.jpeg'
-import usFour from './hokkaido/usFour.jpeg'
-import vanSuitup from './hokkaido/vanSuitup.jpeg'
 
 import nankoweep from './grandCanyon/nankoweep.JPG'
-import bigWall from './grandCanyon/bigWall.JPG'
-import caveBeach from './grandCanyon/caveBeach.JPG'
-import colorfulCampsite from './grandCanyon/colorfulCampsite.JPG'
-import flatCanyon from './grandCanyon/flatCanyon.JPG'
-import floating from './grandCanyon/floating.JPG'
-import hike from './grandCanyon/hike.JPG'
-import lava from './grandCanyon/lava.jpg'
-import littleColorado from './grandCanyon/littleColorado.JPG'
-import overRafts from './grandCanyon/overRafts.JPG'
-import rafting from './grandCanyon/rafting.jpg'
-import sideCanyon from './grandCanyon/sideCanyon.JPG'
 
 import yurtOverlook from './baldyKnoll/yurtOverlook.jpg'
-import antLine from './baldyKnoll/antLine.JPG'
-import coleDaffy from './baldyKnoll/coleDaffy.JPG'
-import coleSpine from './baldyKnoll/coleSpine.JPG'
-import coleWaves from './baldyKnoll/coleWaves.JPG'
-import mountainSunset from './baldyKnoll/mountainSunset.JPG'
-import yurtSunset from './baldyKnoll/yurtSunset.jpg'
 
 import northernLights from './severnRiver/northernLights.jpg'
-import riverSouth from './severnRiver/riverSouth.jpg'
-import riverOverlook from './severnRiver/riverOverlook.jpg'
-import paddleSync from './severnRiver/paddleSync.jpg'
-import smokeyPaddle from './severnRiver/smokeyPaddle.jpg'
-import sunriseGlass from './severnRiver/sunriseGlass.jpg'
-import purpleSky from './severnRiver/purpleSky.jpg'
-import tentCamo from './severnRiver/tentCamo.jpg'
-import rainbowRapid from './severnRiver/rapidRainbow.jpg'
-import russPancake from './severnRiver/russPancake.jpg'
-import russWhitewater from './severnRiver/russWhitewater.jpg'
-import severnLake from './severnRiver/severnLake.jpg'
-import marsClassic from './severnRiver/marsClassic.jpg'
-import marsFloor from './severnRiver/marsFloor.jpg'
-import marsRapid from './severnRiver/marsRapid.jpg'
-import riverCorridor from './severnRiver/riverCorridor.jpg'
-import riverSunset from './severnRiver/riverSunset.jpg'
 
 import doubleRainbow from './ashweigRiver/doubleRainbow.jpg'
-import bonFire from './ashweigRiver/bonFire.jpg'
-import bigHole from './ashweigRiver/bigHole.jpg'
-import canoeLanding from './ashweigRiver/canoeLanding.jpg'
-import pipestoneRapid from './ashweigRiver/pipestoneRapid.jpg'
-import ashewigReflections from './ashweigRiver/asheweigReflections.jpg'
-import orangeSky from './ashweigRiver/orangeSky.jpg'
-import flintstone from './ashweigRiver/flintstone.jpg'
-import asheweigStaff from './ashweigRiver/asheweigStaff.jpg'
-import polarBear from './ashweigRiver/polarBear.jpg'
 
 import grandFalls from './dumoineRiver/grandFalls.jpg'
-import dumoineRapid from './dumoineRiver/dumoineRapid.jpg'
-import reedPaddling from './dumoineRiver/reedPaddling.jpg'
-import churchWoods from './dumoineRiver/churchWoods.jpg'
-import coltonAlex from './dumoineRiver/coltonAlex.jpg'
-import fallsPhoto from './dumoineRiver/fallsPhoto.jpg'
-import cliffView from './dumoineRiver/cliffView.jpg'
 
 import mooseSunset from './missinaibiRiver/mooseSunset.jpg'
-import pictographs from './missinaibiRiver/pictographs.jpg'
-import grifAfter from './missinaibiRiver/grifAfter.jpg'
-import gabeWhitewater from './missinaibiRiver/gabeWhitewater.jpg'
-import grifWhitewater from './missinaibiRiver/grifWhitewater.jpg'
-import tentMosquitos from './missinaibiRiver/tentMosquitos.jpg'
-import grahamBow from './missinaibiRiver/grahamBow.jpg'
-import thunderhouse from './missinaibiRiver/thunderhouse.jpg'
 
-import beachLanding from './mistassibiRiver/beachLanding.jpg'
-import henryMountains from './mistassibiRiver/henryMountains.jpg'
 import hecklauJack from './mistassibiRiver/hecklauJack.jpg'
-import riverMountains from './mistassibiRiver/riverMountains.jpg'
-import andrewPaxton from './mistassibiRiver/andrewPaxton.jpg'
-import lastMistassibi from './mistassibiRiver/lastMistassibi.jpg'
 
 import eustacheLake from './petawawaRiver/eustacheLake.jpg'
-import lakeZoom from './petawawaRiver/lakeZoom.jpg'
-import rollawayRapid from './petawawaRiver/rollawayRapid.jpg'
-import petEnd from './petawawaRiver/petEnd.jpg'
 
-import rupertLights from './rupertRiver/rupertLights.jpg'
 import rupertRainbow from './rupertRiver/rupertRainbow.jpg'
-import oatmealRapids from './rupertRiver/oatmealRapids.jpg'
-import rupertGroup from './rupertRiver/rupertGroup.jpg'
-import mistassiniPaddle from './rupertRiver/mistassiniPaddle.jpg'
 
 import raftBeach from './mainSalmon/raftBeach.JPG'
-import salmonRapid from './mainSalmon/salmonRapid.JPG'
-import acidDay from './mainSalmon/acidDay.JPG'
-import comeDown from './mainSalmon/comeDown.JPG'
 
-import gangSkin from './plummerCanyon/gangSkin.jpg'
-import mountWow from './plummerCanyon/mountWow.jpg'
-import silasSkin from './plummerCanyon/silasSkin.jpg'
-import yurtGang from './plummerCanyon/yurtGang.JPG'
-import plummerMorning from './plummerCanyon/plummerMorning.jpg'
 import plummerSunset from './plummerCanyon/plummerSunset.jpg'
 
-import cabinField from './jacksonPeak/cabinField.jpg'
-import nolanPeak from './jacksonPeak/nolanPeak.jpg'
-import valleySkin from './jacksonPeak/valleySkin.jpg'
-import cabinChop from './jacksonPeak/cabinChop.jpg'
-import karlHill from './jacksonPeak/karlHill.jpg'
 import tourCache from './jacksonPeak/tourCache.jpg'
-import gashView from './jacksonPeak/gashView.jpg'
 
 import selwayAbove from './selwayRiver/selwayAbove.jpg'
-import mooseCreek from './selwayRiver/mooseCreek.jpg'
-import selwayAirstrip from './selwayRiver/selwayAirstrip.jpg'
-import littleNiagra from './selwayRiver/littleNiagra.jpg'
-import ladle from './selwayRiver/ladle.jpg'
-import selwayCrew from './selwayRiver/selwayCrew.jpg'
-import wolfCreek from './selwayRiver/wolfCreek.jpg'
-import selwayBeach from './selwayRiver/selwayBeach.jpg'
-import selwayFalls from './selwayRiver/selwayFalls.jpg'
 
 const selwayPics = [
     selwayAbove,
-    mooseCreek,
-    selwayAirstrip,
-    littleNiagra,
-    ladle,
-    selwayCrew,
-    wolfCreek,
-    selwayBeach,
-    selwayFalls
 ]
 
 const jacksonPeakPics = [
-    cabinField,
-    nolanPeak,
-    valleySkin,
-    cabinChop,
-    karlHill,
     tourCache,
-    gashView
 ]
 
 const plummerCanyonPics = [
     plummerSunset,
-    gangSkin,
-    mountWow,
-    silasSkin,
-    yurtGang,
-    plummerMorning
 ]
 
 const jacksonLakePics = [
     warmSprings,
-    graceFishing,
-    desertRiver,
-    sandLookout,
-    lakeEntrance,
-    stillPaddle,
-    babsRainbow,
-    rainPaddle,
-    moranSunset,
-    lorraineWave,
-    graceSelfie
 ]
 
 const middleForkPics = [
     landscape,
-    tappandRanch,
-    pistol,
-    wall,
-    overlook,
-    hikeDown,
-    boatsOnBeach,
-    smallRapid,
-    deepWater,
-    veilFalls,
-    launchDay
 ]
 
 const shoshonePics = [
     shoshoneOpening, 
-    geyserBasin, 
-    heartCauldron, 
-    ianFishing,
-    kateHungover,
-    katePaddling,
-    lewisLake,
-    lilChannel,
-    silasFihing,
-    sunrise
 ]
 const commissaryPics = [
     yurtMorning,
-    beardsSkin,
-    beardsSummit,
-    crewSkiing,
-    hillaryMe,
-    ridgeSkin
 ]
 
 const hokkaidoPics = [
     sunsetSummit,
-    afterSkiing,
-    andrewKicker,
-    coleLaughing,
-    furanoAlley,
-    henrySmoke,
-    snowBridge,
-    threeBoys,
-    usFour,
-    vanSuitup
 ]
 
 const gcPics = [
     nankoweep,
-    bigWall,
-    caveBeach,
-    colorfulCampsite,
-    flatCanyon,
-    floating,
-    lava,
-    hike,
-    littleColorado,
-    overRafts,
-    rafting,
-    sideCanyon
 ]
+
 const baldyPics = [
     yurtOverlook,
-    antLine,
-    coleDaffy,
-    coleSpine,
-    coleWaves,
-    mountainSunset,
-    yurtSunset
 ]
+
 const severnPics = [
     northernLights,
-    riverSouth,
-    riverOverlook,
-    paddleSync,
-    smokeyPaddle,
-    sunriseGlass,
-    purpleSky,
-    tentCamo,
-    rainbowRapid,
-    russPancake,
-    russWhitewater,
-    severnLake,
-    marsClassic,
-    marsFloor,
-    marsRapid,
-    riverCorridor,
-    riverSunset
 ]
+
 const asheweigPics = [
     doubleRainbow,
-    bonFire,
-    bigHole,
-    canoeLanding,
-    pipestoneRapid,
-    ashewigReflections,
-    orangeSky,
-    flintstone,
-    asheweigStaff,
-    polarBear,
 ]
 
 const dumoinePics = [
     grandFalls,
-    dumoineRapid,
-    reedPaddling,
-    churchWoods,
-    coltonAlex,
-    fallsPhoto,
-    cliffView
 ]
 
 const missinaibiPics = [
-    pictographs,
     mooseSunset,
-    grifAfter,
-    gabeWhitewater,
-    grifWhitewater,
-    tentMosquitos,
-    grahamBow,
-    thunderhouse,
 ]
 
 const mistassibiPics = [
     hecklauJack,
-    beachLanding,
-    henryMountains,
-    riverMountains,
-    andrewPaxton,
-    lastMistassibi
 ]
 
 const petPics = [
     eustacheLake,
-    lakeZoom,
-    rollawayRapid,
-    petEnd
 ]
 
 const rupertPics = [
-    rupertLights,
     rupertRainbow,
-    oatmealRapids,
-    rupertGroup,
-    mistassiniPaddle
 ]
 
 const mainSalmonPics = [
     raftBeach,
-    salmonRapid,
-    acidDay,
-    comeDown
 ]
 
 export { 
